a6e82b7518c1b0cf3be51acdfbfc506647151862
mvmatt605 <112832337+mvmatt605@users.noreply.github.com>
2023-04-12 18:34:13 -0400

    Update ExpNode.java

--- ExpNode.java
+++ ExpNode.java
@@ -1,40 +1,38 @@
 public class ExpNode extends Node {
-
-	private Node left:
-	private Node right;
-	private OpNode op;
-
-	public ExpNode(Node left, Opnode, Node right){
-		this.left = left;
-		this.op = op;
-		this.right = right;
-	}
-
-	@Override
-	public String getName(){
-		return op.getName();
-	}
-
-	@Override
-	public int eval(){
-		int leftVal = left.eval();
-		int rightVal = right.eval();
-		String Operator = op.getName();
-
-		if(operator.equals("+")){
-			return leftVal + rightVal;
-		}
-	
-		if(operator.equals("-")){
-			return leftVal - rightVal;
-		}
-
-		if(operator.equals("*")){
-			return leftVal * rightVal;
-		}
-
-		if(operator.equals("/")){
-			 return leftVal / rightVal;
-		}
-	}
+    
+    @Override
+    public int eval() {
+        if (children().size() == 1) {
+            // if there is only one child, evaluate and return it
+            return children().get(0).eval();
+        } else {
+            // evaluate the first child
+            int result = children().get(0).eval();
+            
+            // loop through remaining children two at a time
+            for (int i = 1; i < children().size(); i += 2) {
+                // the first child of each pair is an OpNode
+                OpNode op = (OpNode) children().get(i);
+                // evaluate the second child of the pair to get a value
+                int value = children().get(i + 1).eval();
+                
+                // update the result according to the operator and value
+                if (op.op.equals("+")) {
+                    result += value;
+                } else if (op.op.equals("-")) {
+                    result -= value;
+                } else if (op.op.equals("*")) {
+                    result *= value;
+                } else if (op.op.equals("/")) {
+                    result /= value;
+                } else {
+                    throw new IllegalArgumentException("Unknown operator: " + op.op);
+                }
+            }
+            
+            // return the final result
+            return result;
+        }
+    }
+    
 }
